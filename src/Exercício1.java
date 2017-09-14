408f633adeaf9c3e10eac7c60f0a80c0f25791e1
aluno <aluno@MSCGRESL04023.estacio.acad>
2017-09-14 19:42:33 -0400

    Teste de Concatenação

---
+++ src/Exercício1.java
@@ -0,0 +1,29 @@
+COMPILAÇÃO NO JAVA
+Na linguagem Java o processo de compilação ocorre da seguinte maneira:
+
+1.    O código fonte (extensão .java) é compilado utilizando o javac (Java Compiler) e armazenado em um arquivo de extensão .class, também conhecido como bytecode.
+
+2.    Em seguida a JVM (Java Virtual Machine), interpreta os bytecodes produzidos pelo compilador.
+
+3.    O JIT (Just-In-Time) converte, em tempo de execução, o código em bytecode para código de máquina.
+
+4.    Por ultimo, fica a cargo do Sistema Operacional em questão executar o programa./*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercício1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
